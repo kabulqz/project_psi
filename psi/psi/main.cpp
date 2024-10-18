f10dfe6f0e60ceb0589fdae29884b6ee386efeee
kabulqz <123zelek@gmail.com>
2024-10-18 19:16:07 +0200

    added SQLite
    for now it connects to the DB, but nothing more

--- psi/psi/main.cpp
+++ psi/psi/main.cpp
@@ -1,11 +1,27 @@
-#include<iostream>
-#include<SFML/Graphics.hpp>
+#include    <iostream>
+#include    <SFML/Graphics.hpp>
+#include    <sqlite3.h>
 
 using namespace std;
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(640, 480), "Project_PSI");
+    //database
+    sqlite3* DB;
+    int exit = sqlite3_open("test.db", &DB);
+
+    if (exit)
+    {
+        cerr << "Error opening SQLite database" << sqlite3_errmsg(DB) << endl;
+        return exit;
+    }
+
+    cout << "Opened SQLite database successfully" << endl;
+    sqlite3_close(DB);
+
+
+    //SFML window
+    sf::RenderWindow window(sf::VideoMode(800, 600), "Project_PSI");
 
     while (window.isOpen())
     {
