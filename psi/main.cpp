13b94f480a4b7baee99ac74476e0981f1b4d5df5
kabulqz <123zelek@gmail.com>
2024-10-19 12:58:04 +0200

    updated files placement and added only a showcase how to use SFML-audio

--- psi/main.cpp
+++ psi/main.cpp
@@ -1,6 +1,7 @@
-#include <iostream>
 #include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
 #include <sqlite3.h>
+#include <iostream>
 
 using namespace std;
 
@@ -12,23 +13,45 @@ int main()
 
     if (exit)
     {
-        cerr << "Error opening SQLite database" << sqlite3_errmsg(DB) << endl;
+        cerr << "Error opening SQLite database\n" << sqlite3_errmsg(DB);
         return exit;
     }
 
-    cout << "Opened SQLite database successfully" << endl;
+    cout << "Opened SQLite database successfully\n";
     sqlite3_close(DB);
 
 
-    //SFML window
+    //SFML window handler
     sf::RenderWindow window(sf::VideoMode(800, 600), "Project_PSI");
 
+    //SFML audio buffer
+    sf::SoundBuffer buffer;
+    if (!buffer.loadFromFile("src/APD.wav"))
+    {
+        //error handling
+        return -1;
+    }
+
+    //SFML audio player
+    sf::Sound sound;
+    sound.setBuffer(buffer);
+    sound.setVolume(10);
+    sound.play();
+
+    //SFML window main loop
     while (window.isOpen())
     {
         sf::Event event;
         while (window.pollEvent(event))
         {
-            
+            if (event.type == sf::Event::Closed)
+            {
+                cout << "The window has been closed\n";
+                window.close();
+            }
+
+
+
         }
 
         window.clear();
