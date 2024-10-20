797da5175b2578dc930ba54b3b470fa47fd91974
kabulqz <123zelek@gmail.com>
2024-10-20 13:52:07 +0200

    icon, cursor, mouseClick

--- psi/main.cpp
+++ psi/main.cpp
@@ -1,4 +1,5 @@
 #include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
 #include <SFML/Audio.hpp>
 #include <sqlite3.h>
 #include <iostream>
@@ -7,56 +8,81 @@ using namespace std;
 
 int main()
 {
-    //database
-    sqlite3* DB;
-    int exit = sqlite3_open("test.db", &DB);
+	//database
+	sqlite3* DB;
+	int exit = sqlite3_open("test.db", &DB);
 
-    if (exit)
-    {
-        cerr << "Error opening SQLite database\n" << sqlite3_errmsg(DB);
-        return exit;
-    }
+	if (exit)
+	{
+		cerr << "Error opening SQLite database\n" << sqlite3_errmsg(DB);
+		return exit;
+	}
 
-    cout << "Opened SQLite database successfully\n";
-    sqlite3_close(DB);
-
-
-    //SFML window handler
-    sf::RenderWindow window(sf::VideoMode(800, 600), "Project_PSI");
-
-    //SFML audio buffer
-    sf::SoundBuffer buffer;
-    if (!buffer.loadFromFile("src/APD.wav"))
-    {
-        //error handling
-        return -1;
-    }
-
-    //SFML audio player
-    sf::Sound sound;
-    sound.setBuffer(buffer);
-    sound.setVolume(10);
-    sound.play();
-
-    //SFML window main loop
-    while (window.isOpen())
-    {
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
-            {
-                cout << "The window has been closed\n";
-                window.close();
-            }
+	cout << "Opened SQLite database successfully\n";
+	sqlite3_close(DB);
 
 
+	//SFML window handler
+	sf::RenderWindow window(sf::VideoMode(800, 600), "Project_PSI");
 
-        }
+	//SFML window icon
+	sf::Image icon;
+	if (!icon.loadFromFile("src/icon.png"))
+	{
+		return -1;
+	}
 
-        window.clear();
-        window.display();
-    }
+	window.setIcon(icon.getSize().x, icon.getSize().y, icon.getPixelsPtr());
 
-    return 0;
+	//SFML cursor & cursor style
+	sf::Cursor cursor;
+	sf::Image cursorImage;
+	if (cursorImage.loadFromFile("src/cursor.png"))
+	{
+		if(cursor.loadFromPixels(cursorImage.getPixelsPtr(), sf::Vector2u(16, 16), sf::Vector2u(0, 0)))
+		{
+			window.setMouseCursor(cursor);
+		}
+	}
+
+	//SFML audio buffer
+	sf::SoundBuffer buffer;
+	if (!buffer.loadFromFile("src/click3.ogg"))
+	{
+		//error handling
+		return -1;
+	}
+
+	//SFML audio player
+	sf::Sound mouseClick;
+	mouseClick.setBuffer(buffer);
+	mouseClick.setVolume(10);
+	
+
+	//SFML window main loop
+	while (window.isOpen())
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+			{
+				cout << "The window has been closed\n";
+				window.close();
+			}
+			else if (event.type == sf::Event::MouseButtonPressed)
+			{
+				//click3.ogg
+				mouseClick.play();
+			}
+
+
+
+		}
+
+		window.clear();
+		window.display();
+	}
+
+	return 0;
 }
